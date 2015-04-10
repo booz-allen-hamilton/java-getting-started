ec1ac0926523e111d915d365cb0853c7c0aba80b
Nirmal Mehta <nirmalkmehta@gmail.com>
2015-04-10 15:25:26 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -61,7 +61,7 @@ public class Main extends HttpServlet {
   }
 
   public static void main(String[] args) throws Exception {
-    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    Server server = new Server(Integer.valueOf("9000");
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
     server.setHandler(context);
